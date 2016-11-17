0aed7b61c02673c8f46c2f5dc76e4dede0c8a5d7
Louis Gaume <Louis Gaume>
2016-11-17 22:46:09 +0100

    Select ajout semestre

--- Semestre/models.py
+++ Semestre/models.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from Diplome.models import Diplome
 # Create your models here.
 
 
 class Semestre(models.Model):
 	code = models.CharField(max_length=30,null=False)
+	diplome = models.ForeignKey('Diplome.Diplome', null=True)
 	def __str__(self):
 		return self.code
