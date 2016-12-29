dda3f1c3c5d15e0156db2b3956ac370bdf439733
EtiennePericat <etienne.pericat.etu@mail.com>
2016-12-29 22:16:36 +0100

    efzefzefzef
    Signed-off-by: EtiennePericat <etienne.pericat.etu@æmail.com>

--- Semestre/models.py
+++ Semestre/models.py
@@ -11,4 +11,4 @@ class Semestre(models.Model):
 	code = models.CharField(max_length=30,null=False)
 	diplome = models.ForeignKey(Diplome, null=True)
 	def __str__(self):
-		return self.code
+		return self.code.encode('utf8')
