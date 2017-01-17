125d5b1c54402150e625e45c0d3a5f0201c713cb
alex-47 <alexandre.ternois@gmail.com>
2017-01-17 14:48:03 +0100

    egrnh

--- Documents/views.py
+++ Documents/views.py
@@ -1,56 +1,69 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
-
 from xlutils.copy import copy # http://pypi.python.org/pypi/xlutils
 from xlrd import open_workbook # http://pypi.python.org/pypi/xlrd
 from xlwt import easyxf
+import xlwt
 from Etudiant.models import Etu
-from Etudiant.models import Etu
+from UE.models import UE
+from Note.models import Resultat_UE
+from Note.models import Resultat_Semestre
 from Matiere.models import Matiere
+from Semestre.models import Semestre
 from Note.models import Note
-from Etudiant.forms import SelectEtu
+from UE.forms import SelectSemestre
 # Create your views here.
 
 """Cette vue permet de generer les documents"""
 def genererDocuments(request):
 	if request.method == 'POST':  
-		if not request.session['etu']:
-			Etudiants = Etu.objects.all()
-			form = SelectEtu(request.POST, etus=Etudiants)
-			if form.is_valid() :
-				id_etu = form.cleaned_data['select']
-				request.session['id_etu'] = id_etu
-				request.session['etu'] = True
-			#book = Workbook()
-			
-			book = open_workbook('DocJury/S2.xls',formatting_info=True)
-			book.sheet_by_index(0)
-			newFeuille = copy(book)
-			etus = Etu.objects.all()
-			ligne = 7
-			cp =0
-			for etu in etus:
-				colonne = 0
-				newFeuille.get_sheet(0).write(ligne,colonne,cp)
+		semestre = Semestre.objects.all()
+		form = SelectSemestre(request.POST, semestres=semestre)
+		if form.is_valid() :
+			request.session['id_semestre'] = form.cleaned_data['select']
+		#book = Workbook()
+		style = xlwt.easyxf(' alignment: horizontal center, vertical center; borders: left thin, right thin, top thin, bottom thin;')
+		book = open_workbook('DocJury/S2.xls',formatting_info=True)
+		book.sheet_by_index(0)
+		newFeuille = copy(book)
+		etus = Etu.objects.all()
+		ligne = 7
+		cp =0
+		for etu in etus:
+			colonne = 0
+			newFeuille.get_sheet(0).write(ligne,colonne,cp, style)
+			colonne +=1
+			newFeuille.get_sheet(0).write(ligne,colonne,etu.apogee, style)
+			colonne +=1
+			newFeuille.get_sheet(0).write(ligne,colonne,etu.nom, style)
+			colonne +=1
+			newFeuille.get_sheet(0).write(ligne,colonne,etu.prenom, style)
+			colonne +=2
+			semestre = Semestre.objects.all().filter(id=request.session['id_semestre'])
+			print(semestre)
+			ues = UE.objects.all().filter(semestre=semestre)
+			for ue in ues:
+				try:
+					noteUE = Resultat_UE.objects.get(etudiant=etu, ue=ue)	
+					newFeuille.get_sheet(0).write(ligne,colonne,noteUE.note, style)
+					colonne +=1
+				except Resultat_UE.DoesNotExist:
+					print("probleme")
+			try:
+				resS = Resultat_Semestre.objects.get(etudiant = etu, semestre=semestre)
+				newFeuille.get_sheet(0).write(ligne,colonne,resS.note, style)
 				colonne +=1
-				newFeuille.get_sheet(0).write(ligne,colonne,etu.apogee)
-				colonne +=1
-				newFeuille.get_sheet(0).write(ligne,colonne,etu.nom)
-				colonne +=1
-				newFeuille.get_sheet(0).write(ligne,colonne,etu.prenom)
-				colonne +=1
-				newFeuille.get_sheet(0).write(ligne,colonne,"12,5")
-				colonne +=1
-				newFeuille.get_sheet(0).write(ligne,colonne,"12,5")
-				colonne +=1
-				newFeuille.get_sheet(0).write(ligne,colonne,"12,5")
-				ligne += 1
-				cp+=1
-			newFeuille.save('output.xls')
-	else :
-		Etudiants = Etu.objects.all()
-		request.session['etu'] = False
-		form = SelectEtu(etus=Etudiants)
+				newFeuille.get_sheet(0).write(ligne,colonne,resS.resultat, style)
+			except Resultat_Semestre.DoesNotExist:
+				print("probleme")
+			ligne += 1
+			cp+=1
+			res=True
+		newFeuille.save('output.xls')
+	else:
+		res=False
+		semestre = Semestre.objects.all()
+		form = SelectSemestre(semestres=semestre)
 	return render(request, 'contenu_html/genererDocuments.html', locals())
 
 
